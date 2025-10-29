401aa5f55533ba64dd00040df289fc67bf47d692
Tu Nombre <bzuniga@unbcorp.cl>
2025-10-29 13:42:05 -0300

    Revertir botÃ³n Reiniciar a funcionalidad original simple

--- resources/views/livewire/admin/results.blade.php
+++ resources/views/livewire/admin/results.blade.php
@@ -118,12 +118,3 @@
 
     @livewire('show-codes')
 </main>
-
-<script>
-document.addEventListener('livewire:init', () => {
-    Livewire.on('executeResetProcess', (event) => {
-        console.log('Ejecutando proceso de recálculo...');
-        // El proceso se ejecuta automáticamente en el backend
-    });
-});
-</script>
